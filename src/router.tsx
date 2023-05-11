1753d045a9350fc244dbfe9bfb89d17697ccd475
ghazal <ghazal.abaszade1@gmail.com>
2023-05-11 12:51:20 +0330

    created layout and added to router

--- src/router.tsx
+++ src/router.tsx
@@ -1,16 +1,22 @@
 import {createBrowserRouter} from "react-router-dom";
 import Login from "./features/Identity/components/login";
 import Register from "./features/Identity/components/register";
+import IdentityLayout from "./layouts/identity-layout";
 
 const router = createBrowserRouter([
     {
-        path: 'login',
-        element: <Login />
-    },
-    {
-        path: 'login',
-        element: <Register />
-    },
+        element: <IdentityLayout />,
+        children: [
+            {
+                path: 'login',
+                element: <Login />
+            },
+            {
+                path: 'register',
+                element: <Register />
+            }
+        ]
+    }
 ])
 
 export default router;
