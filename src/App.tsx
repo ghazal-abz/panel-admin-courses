cd2f7d447a9eff200529dbef08942c868aa7a338
ghazal <ghazal.abaszade1@gmail.com>
2023-05-21 17:00:34 +0330

    added dynamic link for changing theme

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,21 @@
 import {RouterProvider} from "react-router-dom"
 import router from "./router";
 import './services/i18n.js'
+import {useAppContext} from "./contexts/app/app-context";
+import {useEffect} from "react";
 
 function App() {
+    const {theme} = useAppContext();
 
+    useEffect(() => {
+        const head = document.head;
+        const link = document.createElement('link');
+        link.rel = "stylesheet";
+        link.href= `/css/${theme}.css`;
+        head.appendChild(link);
+
+        return () => {head.removeChild(link)}
+    },[theme])
     return (
         <RouterProvider router={router} />
     )
