dd3202b8aa8ea4135f2906deef77e856f63bd51e
ghazal <ghazal.abaszade1@gmail.com>
2023-05-21 14:55:04 +0330

    created aap context for changing language

--- src/components/change-language.tsx
+++ src/components/change-language.tsx
@@ -1,13 +1,19 @@
 import usFlag from "@assets/images/us.png";
 import faFlag from "@assets/images/fa.png";
-import { useTranslation } from "react-i18next";
+import {useTranslation} from "react-i18next";
 import {useEffect, useRef, useState} from "react";
+import {useAppContext} from "../contexts/app/app-context";
+
 const ChangeLanguage = () => {
     const [show, setShow] = useState(false);
     const ref = useRef();
 
+    const {changeLanguage, language} = useAppContext();
+
+    const {t} = useTranslation();
+
     useEffect(() => {
-        const checkIfClickOutside = (e) => {
+        const checkIfClickOutside = e => {
             if (show && ref.current && !ref.current.contains(e.target)) {
                 setShow(false);
             }
@@ -26,16 +32,16 @@ const ChangeLanguage = () => {
                 className="nav-flag dropdown-toggle"
                 onClick={() => setShow(true)}
             >
-                {/*<img src={language === 'fa' ? faFlag : usFlag} alt="English" />*/}
+                <img src={language === 'fa' ? faFlag : usFlag} alt="English" />
             </a>
             <div ref={ref} className={`dropdown-menu dropdown-menu-end ${show ? 'show' : undefined}`}>
                 <a className="dropdown-item fw-bolder d-flex align-items-center gap-2">
                     <img src={faFlag} width="20" />
-                    <span className="align-middle ">فارسی</span>
+                    <span className="align-middle ">{t("dropDown.persian")}</span>
                 </a>
                 <a className="dropdown-item fw-bolder d-flex align-items-center gap-2">
                     <img src={usFlag} width="20" />
-                    <span className="align-middle">English</span>
+                    <span className="align-middle">{t("dropDown.english")}</span>
                 </a>
             </div>
         </div>
