8a06c01a51ee380c789af57d067f1a1acb6d36c7
ghazal <ghazal.abaszade1@gmail.com>
2023-05-21 14:57:13 +0330

    changed syntax

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
 // import Login from "./features/Identity/components/register";
 import {RouterProvider} from "react-router-dom"
 import router from "./router";
+import './services/i18n.js'
 
 function App() {
 
     return (
-        <RouterProvider router={router}>
-
-        </RouterProvider>
+        <RouterProvider router={router} />
     )
 }
 
