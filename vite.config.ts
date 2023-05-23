7a45dd19b5112400029786e7694ebd9d39d32d90
ghazal <ghazal.abaszade1@gmail.com>
2023-05-23 14:44:30 +0330

    added alias for feature path

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   resolve: {
     alias: {
-      '@assets': path.resolve(__dirname, './src/assets'),
-      '@services': path.resolve(__dirname, './src/services')
-    }
+      "@assets": path.resolve(__dirname, "./src/assets"),
+      "@services": path.resolve(__dirname, "./src/services"),
+      "@features": path.resolve(__dirname, "./src/features"),
+    },
   },
   plugins: [react()],
-})
+});
