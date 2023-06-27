9e036a71a3616c9db2b8fc412fd8804ca7633791
ghazal <ghazal.abaszade1@gmail.com>
2023-06-27 16:18:13 +0330

    create pagination for category list

--- src/pages/course-categories.tsx
+++ src/pages/course-categories.tsx
@@ -29,14 +29,20 @@ const CourseCategories = () => {
   );
 };
 
-export async function categoriesLoader() {
+export async function categoriesLoader(request) {
   return defer({
-    categories: loadCategories(),
+    categories: loadCategories(request),
   });
 }
 
-const loadCategories = async () => {
-  const response = await httpInterceptedService.get("/CourseCategory/sieve");
+const loadCategories = async (request) => {
+  const page = new URL(request.url).searchParams.get("page") || 1;
+  const pageSize = import.meta.env.VITE_PAGE_SIZE;
+  let url = "/CourseCategory/sieve";
+
+  url += `?page=${page}&pageSize=${pageSize}`;
+
+  const response = await httpInterceptedService.get(url);
   return response.data;
 };
 
